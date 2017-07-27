624ba139f57baa3d6e8f60fc9a6c7edd5ab26a93
twelveouncemouse <vasilyev.rw@gmail.com>
2017-07-27 15:47:16 +0300

    Update ServerProcess.h
    commented code removed

--- p6/src/ServerProcess.h
+++ p6/src/ServerProcess.h
@@ -35,20 +35,12 @@ private:
 	static void listener_cb(struct evconnlistener *, evutil_socket_t,
 	    struct sockaddr *, int socklen, void *);
 	static void signal_cb(evutil_socket_t sig, short events, void *user_data);
-	//void init_ipc();
-	//void shutdown_workers();
-	//void release_ipc();
 
 	void send_fd_to_worker(int fd);
 
-	//void process_response_from_worker(CommandMessage* cmd_msg);
-	//void send_response_to_client(std::string& response, int connection_id);
 
 	const int port;
 	struct event_base* base;
-//	int message_queue_id;
-//	int shared_mem_id;
-//	int semaphores_id;
 };
 
 #endif /* SERVERPROCESS_H_ */
